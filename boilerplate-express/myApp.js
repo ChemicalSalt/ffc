33a65d744cc45bb1c285a0fe81897bd4923dc75b
ChemicalSalt <chemistry50551555@gmail.com>
2025-09-01 20:56:15 +0530

    ffc

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -2,9 +2,16 @@ let express = require('express');
 let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/index.html");
 });
+
+// Serve JSON at /json
+app.get("/json", function(req, res) {
+  res.json({ message: "Hello json" });
+});
+
 module.exports = app;
 
 
