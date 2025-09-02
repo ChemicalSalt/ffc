3558cbad38da2fd5b27293e7c4b5787fa0f25557
ChemicalSalt <chemistry50551555@gmail.com>
2025-09-02 13:17:42 +0530

    ffc

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -2,19 +2,23 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+// Root-level logger middleware
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Serve JSON at /json
 app.get("/json", (req, res) => {
   let msg = "Hello json";                 
   if (process.env.MESSAGE_STYLE === "uppercase") 
     msg = msg.toUpperCase();            
   res.json({ message: msg });          
 });
-
 
 module.exports = app;
