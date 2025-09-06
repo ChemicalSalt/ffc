d9ca922443881158819b3d7d179de2eb7611b16d
ChemicalSalt <chemistry50551555@gmail.com>
2025-09-06 15:32:52 +0530

    ffc

--- boilerplate-project-timestamp/index.js
+++ boilerplate-project-timestamp/index.js
@@ -1,32 +1,46 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+// enable CORS for FCC testing
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+// serve static files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+// root route
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// example API endpoint
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: 'hello API' });
 });
 
+// timestamp microservice
+app.get('/api/:date?', (req, res) => {
+  let dateString = req.params.date;
+  let date;
 
+  if (!dateString) {
+    date = new Date();
+  } else if (!isNaN(Number(dateString))) {
+    date = new Date(parseInt(dateString));
+  } else {
+    date = new Date(dateString);
+  }
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
